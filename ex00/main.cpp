b5eda805c814fb3674e8fd1d18a914cf3d874540
love <tlove.tky@gmail.com>
2023-08-24 12:18:38 +0900

    ex00: テスト実装

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,10 +1,40 @@
-// #include "Bureaucrat.hpp"
+#include "easyfind.hpp"
+#include <iostream>
+#include <vector>
+#include <list>
 
-int main( void )
-{
+int main() {
+	// Using easyfind with std::vector
+	std::vector<int> myVector;
+	myVector.push_back(1);
+	myVector.push_back(2);
+	myVector.push_back(3);
+	myVector.push_back(4);
+	myVector.push_back(5);
 
-	return	0;
+	try{
+		std::vector<int>::iterator it = easyfind(myVector, 2);
+		std::cout << "Found value: " << *it << std::endl;
+	} catch (const std::runtime_error& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	// Using easyfind with std::list
+	std::list<int> myList;
+	myList.push_back(40);
+	myList.push_back(50);
+	myList.push_back(60);
+
+	try {
+		std::list<int>::iterator it = easyfind(myList, 50);
+		std::cout << "Found value: " << *it << std::endl;
+	} catch (const std::runtime_error& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	return 0;
 }
+
 
 __attribute((destructor))
 static void destructor() {
