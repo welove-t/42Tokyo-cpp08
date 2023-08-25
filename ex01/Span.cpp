cb7bef507842ce95d634a5aaddb0ba4815c87dc3
love <tlove.tky@gmail.com>
2023-08-25 10:26:08 +0900

    ex01: Spanクラスの実装（Orthodox Canonical Form）

--- ex01/Span.cpp
+++ ex01/Span.cpp
@@ -1,1 +1,32 @@
 #include "Span.hpp"
+
+/* Orthodox Canonical Form */
+Span::Span()
+{
+	std::cout << "(Span) Default Constructor called" << std::endl;
+}
+
+Span::Span(unsigned int N) :_maxSize(N)
+{
+	std::cout << "(Span) Constructor called" << std::endl;
+}
+
+Span::Span(const Span& rhs) :_vec(rhs._vec), _maxSize(rhs._maxSize)
+{
+	std::cout << "(Span) Copy Constructor called"  << std::endl;
+}
+
+Span::~Span()
+{
+	std::cout << "(Span) Destructor called" << std::endl;
+}
+
+Span& Span::operator=(const Span& rhs)
+{
+	std::cout << "(Span) Copy assignment operator called" << std::endl;
+	if (this == &rhs)
+		return *this;
+	_vec = rhs._vec;
+	_maxSize = rhs._maxSize;
+	return *this;
+}
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -2,11 +2,28 @@
 #define SPAN_HPP
 
 #include <iostream>
+#include <vector>
 
 #define RED		"\e[0;31m" // Red
 #define GREEN	"\e[0;32m" // Green
 #define YELLOW	"\e[0;33m" // Yellow
 #define BLUE	"\e[0;34m" // Blue
 #define RESET 	"\e[0m"    // Reset
+
+class Span
+{
+	private:
+		std::vector<int>	_vec;
+		unsigned int		_maxSize;
+	public:
+		Span();
+		Span(unsigned int N);
+		~Span();
+
+		Span(const Span& rhs);
+		Span& operator=(const Span& rhs);
+
+};
+
 
 #endif
